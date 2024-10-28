0f8cb33d30a9811f09407f507f3ee97621587434
rmdhniki <rmdhniki18@gmail.com>
2024-10-28 23:28:24 +0700

    vidio 6 - view data

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,9 +11,50 @@ Route::get('/about', function () {
     return view('about', ['title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', [
+        'title' => 'Blog', 
+        'posts' => [
+            [
+                'id' => '1',
+                'slug' => 'judul-artikel-1',
+                'title' => 'Judul Artikel 1',
+                'author' => 'Rifki Ramadhani',
+                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam nobis fugit corrupti, quaerat non perspiciatis id est sequi debitis ipsam assumenda aspernatur praesentium vel, reprehenderit nulla doloribus accusamus inventore distinctio.'
+            ],
+            [
+                'id' => '2',
+                'slug' => 'judul-artikel-2',
+                'title' => 'Judul Artikel 2',
+                'author' => 'Rmdhnikii',
+                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam nobis fugit corrupti, quaerat non perspiciatis id est sequi debitis ipsam assumenda aspernatur praesentium vel, reprehenderit nulla doloribus accusamus inventore distinctio.'
+            ]
+        ]
+    ]);
 });
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => '1',
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Rifki Ramadhani',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam nobis fugit corrupti, quaerat non perspiciatis id est sequi debitis ipsam assumenda aspernatur praesentium vel, reprehenderit nulla doloribus accusamus inventore distinctio.'
+        ],
+        [
+            'id' => '2',
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Rmdhniki',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam nobis fugit corrupti, quaerat non perspiciatis id est sequi debitis ipsam assumenda aspernatur praesentium vel, reprehenderit nulla doloribus accusamus inventore distinctio.'
+        ]
+    ];
+    $post = Arr::first($posts, function($post) use ($slug) {
+        return $post['slug'] === $slug;
+    });
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
